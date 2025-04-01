7ab563767630705c29febed13985ab997255afc0
sakthi1043 <sakthi1043@gmail.com>
2025-04-01 00:12:59 -0700

    db commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,39 +1,52 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-require("dotenv").config();
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import authRoute from "./src/routes/auth.route.js";
+//Db connection
+import { connectDB } from "./src/lib/db.js";
 
 const app = express();
+dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+
+app.use(cors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+    allowedHeaders:'Content-Type,Authorization',
+}));
 
 // Dummy user data (Replace with a database in production)
-const users = [
-    { username: "sakthi", password: "sakthi@1043" },
-    { username: "vel", password: "vel@9790" }
-];
+// const users = [
+//     { username: "sakthi", password: "sakthi@1043" },
+//     { username: "vel", password: "vel@9790" }
+// ];
 
-app.get("/",(req,res)=>{
-    res.send("Hello World");
-})
+// app.get("/",(req,res)=>{
+//     res.send("Hello World");
+// })
 
-// Login API
-app.post("/login", (req, res) => {
-    const { username, password } = req.body;
+// // Login API
+// app.post("/login", (req, res) => {
+//     const { username, password } = req.body;
 
-    const user = users.find(u => u.username === username && u.password === password);
+//     const user = users.find(u => u.username === username && u.password === password);
 
-    if (user) {
-        res.json({ success: true, message: "Login successful!",id:1 });
-    } else {
-        res.json({ success: false, message: "Invalid username or password",id:0 });
-    }
-});
+//     if (user) {
+//         res.json({ success: true, message: "Login successful!",id:1 });
+//     } else {
+//         res.json({ success: false, message: "Invalid username or password",id:0 });
+//     }
+// });
 
 // Start Server
+app.use("/api/auth",authRoute);
+
 app.listen(PORT, () => {
+    connectDB();
     console.log(`Server running on http://localhost:${PORT}`);
 });
